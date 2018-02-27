d8b94f258d486f893fb087ea0a118d5bff30bff9
Vinay K Pisharody <vinay.k.pisharody@gmail.com>
2018-02-27 12:18:01 +0000

    DQN Continue from 3905

--- DQN-master/train_agent.py
+++ DQN-master/train_agent.py
@@ -37,8 +37,8 @@ if __name__ == '__main__':
     load_episode=-1
 
     ###CHANGE WHEN RESTARTING FROM A CHECKPOINT###
-    load_episode=3240
-    log_dir1 = "log/2018-02-26_12-22-17_SuperMarioAllStarsDeterministic-v4_False"
+    load_episode=3905
+    log_dir1 = "log/2018-02-26_23-44-05_SuperMarioAllStarsDeterministic-v4_False"
     print(log_dir)
     saver.restore(agent.session,log_dir1+'/episode_%d.ckpt'%(load_episode))
     agent.set_agent()
