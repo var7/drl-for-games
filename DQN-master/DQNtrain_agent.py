c6354c5c29a1314f988af52cc63dbfa715e0f3d1
Vinay K Pisharody <vinay.k.pisharody@gmail.com>
2018-03-10 12:11:16 +0000

    DQN continue from 570

--- DQN-master/DQNtrain_agent.py
+++ DQN-master/DQNtrain_agent.py
@@ -22,12 +22,12 @@ if __name__ == '__main__':
     load_episode=-1
 
     ###CHANGE WHEN RESTARTING FROM A CHECKPOINT###
-    # load_episode=0
-    # log_dir1 = "log/"+dir+"/2018-03-10_00-37-16_SuperMarioAllStarsDeterministic-v4_True"
-    # print(log_dir)
-    # saver.restore(agent.session,log_dir1+'/episode_%d.ckpt'%(load_episode))
-    # agent.set_agent()
-    # agent.load_replay(log_dir1)
+    load_episode=570
+    log_dir1 = "log/"+dir+"/2018-03-10_01-00-39_SuperMarioAllStarsDeterministic-v4_False"
+    print(log_dir)
+    saver.restore(agent.session,log_dir1+'/episode_%d.ckpt'%(load_episode))
+    agent.set_agent()
+    agent.load_replay(log_dir1)
     ###END CHANGE###
     
     load_episode+=1
--- DQN-master/watch_playing.py
+++ DQN-master/watch_playing.py
@@ -9,16 +9,17 @@ import numpy as np
 from agent import QAgent
 from configs import mario_config
 
-load_episode=5850
+load_episode=550
 config=mario_config
 
 
 agent = QAgent(config=config, log_dir=None)
 
-tf.train.Saver().restore(agent.session,'log/2018-03-05_12-09-42_SuperMarioAllStarsDeterministic-v4_False/episode_%d.ckpt'%(load_episode))
+tf.train.Saver().restore(agent.session,'log/DQN/2018-03-10_01-00-39_SuperMarioAllStarsDeterministic-v4_False/episode_%d.ckpt'%(load_episode))
 agent.set_agent()
-# eps=agent.get_epsilon()
+eps=agent.get_epsilon()
+print(agent.session.run(agent.training_reward))
+print(eps)
 print(agent.get_steps())
-# print(eps)
-# scores = [agent.validate_episode(epsilon=0.5, visualise=True) for i in range(config['episodes_validate_runs'])] # WITH VALIDATION
-# print(scores)
+scores = [agent.validate_episode(epsilon=0.1, visualise=True) for i in range(config['episodes_validate_runs'])] # WITH VALIDATION
+print(scores)
